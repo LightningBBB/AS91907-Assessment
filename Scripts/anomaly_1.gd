aab20f58f2d87cf073ffe7ec9935742cd760dd65
LightningBB <Crazypenguin919@gmail.com>
2026-05-14 10:08:02 +1200

    v0.3.1
    - Continued work on path-tracing for anomaly_1
    
    BUGS
    - Fix link to navigation region node in anomaly_1 script

--- Scripts/anomaly_1.gd
+++ Scripts/anomaly_1.gd
@@ -2,5 +2,41 @@ extends CharacterBody2D
 
 @onready var agent = $NavigationAgent2D
 @onready var timer = $Timer
+@onready var region = $"../NavigationRegion2D"
 
 var speed = 10
+
+var direction
+var next_position
+
+func _ready() -> void:
+	timer.timeout.connect(pick_new_target)
+	
+	pick_new_target()
+	
+	next_position = agent.get_next_path_position()
+	direction = (next_position - global_position).normalized()
+	
+func _process(_delta: float) -> void:
+	if agent.is_navigation_finished():
+		velocity = Vector2.ZERO
+		
+		next_position = agent.get_next_path_position()
+		direction = (next_position - global_position).normalized()
+		
+		if timer.is_stopped():
+				timer.start()
+		move_and_slide()
+		return
+		
+	velocity = direction * speed
+	move_and_slide()
+	
+func pick_new_target():
+	var random_point = NavigationServer2D.region_get_random_point(
+		region.get_rid(),
+		1,
+		false
+	)
+
+	agent.target_position = random_point
