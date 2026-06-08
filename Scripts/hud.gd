d2176838ffcd51b54d47479f5d18e0f2973cbeb2
LightningBB <Crazypenguin919@gmail.com>
2026-06-08 14:16:39 +1200

    v1.0.1
    - Added progress bar for fuel
    - Very basic title screen
    - Very basic death screen

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -1,6 +1,15 @@
 extends CanvasLayer
 
 @onready var fuel_bar = $Control/ProgressBar
+@onready var prototype_goal = $Control/PrototypeGoal
+@onready var death_label = $Control/Death
 
 func _process(_delta):
 	fuel_bar.value = Global.fuel
+	if Global.fuel == 0:
+		death_label.show()
+
+func _ready() -> void:
+	await get_tree().create_timer(3.0).timeout
+	
+	prototype_goal.hide() 
